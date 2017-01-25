67f622a68e011784dc7082905a1ad62a2c2ecfa7
Sho Nakatani <lay.sakura@gmail.com>
2017-01-25 12:57:14 +0900

    エラーの再現

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-fluentd -c /fluentd/etc/fluent.conf -p /fluentd/plugins &
-/opt/docker/bin/verboseservice &
+traceroute elasticsearch
 
 sleep 10
 
+fluentd -c /fluentd/etc/fluent.conf -p /fluentd/plugins &
+/opt/docker/bin/verboseservice &
+
+sleep 3
+
 cat /etc/hosts
+traceroute elasticsearch
 telnet elasticsearch 9200
-curl http://elasticsearch:9200
+curl -vv http://elasticsearch:9200
 
 tail -f /dev/null
+
