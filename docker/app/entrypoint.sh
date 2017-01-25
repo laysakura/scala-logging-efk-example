d11c3136924c0197781c761b28180d6f8cd3ce54
Sho Nakatani <lay.sakura@gmail.com>
2017-01-25 12:32:41 +0900

    wip

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,4 +3,10 @@
 fluentd -c /fluentd/etc/fluent.conf -p /fluentd/plugins &
 /opt/docker/bin/verboseservice &
 
+sleep 10
+
+cat /etc/hosts
+telnet elasticsearch 9200
+curl http://elasticsearch:9200
+
 tail -f /dev/null
