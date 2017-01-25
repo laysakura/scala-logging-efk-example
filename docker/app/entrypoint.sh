6d30b820ea7d2b983506ba92a061170b4d104442
Sho Nakatani <lay.sakura@gmail.com>
2017-01-25 13:27:24 +0900

    デバッグコード削除

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,10 +3,4 @@
 fluentd -c /fluentd/etc/fluent.conf -p /fluentd/plugins &
 /opt/docker/bin/verboseservice &
 
-sleep 10
-
-cat /etc/hosts
-telnet elasticsearch 9200
-curl http://elasticsearch:9200
-
 tail -f /dev/null
