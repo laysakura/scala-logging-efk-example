27f9a4556d2324bf4ad956e86c6551463bba8044
Sho Nakatani <lay.sakura@gmail.com>
2017-01-26 23:29:58 +0900

    bugfix: log名

---
+++ verboseService/src/test/scala/com/github/laysakura/test/efk/server/EmbeddedVerboseServiceTest.scala
@@ -0,0 +1,32 @@
+package com.github.laysakura.test.efk.server
+
+import com.github.laysakura.efk.idl.{VerboseService, VerboseService$FinagleClient}
+import com.github.laysakura.efk.modules._
+import com.github.laysakura.efk.server.VerboseServiceServer
+import com.twitter.finatra.thrift.{EmbeddedThriftServer, ThriftClient}
+import com.twitter.inject.app.TestInjector
+import com.twitter.inject.server.FeatureTest
+import com.twitter.util.{Await, Future}
+
+class EmbeddedVerboseServiceTest extends FeatureTest {
+  override val injector = TestInjector(
+    modules = Seq(
+      new ClientIdModule("VerboseServiceTest"),
+      VerboseServiceModule, VerboseServiceConfigModule,
+      CalculatorServiceModule, CalculatorServiceConfigModule
+    )
+  )
+
+  override val server = new EmbeddedThriftServer(
+    twitterServer = new VerboseServiceServer
+  ) with ThriftClient
+
+  lazy val client = server.thriftClient[VerboseService[Future]]("EmbeddedVerboseServiceTest")
+
+  "client" should {
+    "successfully call echo() API" in {
+      val res = Await.result(client.echo("hi :D"))
+      res shouldBe "You said: hi :D"
+    }
+  }
+}
