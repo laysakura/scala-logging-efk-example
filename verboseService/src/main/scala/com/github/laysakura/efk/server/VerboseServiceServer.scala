2bbbb3d3434bcdaad4d6b9cea3c92a0659cba016
Sho Nakatani <lay.sakura@gmail.com>
2017-01-26 09:50:27 +0900

    不足injector

--- verboseService/src/main/scala/com/github/laysakura/efk/server/VerboseServiceServer.scala
+++ verboseService/src/main/scala/com/github/laysakura/efk/server/VerboseServiceServer.scala
@@ -1,6 +1,7 @@
 package com.github.laysakura.efk.server
 
 import com.github.laysakura.efk.controllers.VerboseServiceController
+import com.github.laysakura.efk.modules.{CalculatorServiceConfigModule, CalculatorServiceModule, ClientIdModule}
 import com.twitter.finatra.thrift.filters.TraceIdMDCFilter
 import com.twitter.finatra.thrift.ThriftServer
 import com.twitter.finatra.thrift.routing.ThriftRouter
@@ -13,6 +14,11 @@ class VerboseServiceServer extends ThriftServer
   override val name = "verbose-service-server"
   override val defaultFinatraThriftPort: String = s":$THRIFT_PORT"
   override def defaultHttpPort: Int = ADMIN_PORT
+
+  override val modules = Seq(
+    new ClientIdModule("VerboseServiceServer"),
+    CalculatorServiceModule, CalculatorServiceConfigModule
+  )
 
   override def configureThrift(router: ThriftRouter) {
     router
